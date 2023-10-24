a9ee1e426c765eca62817185704341c7071b2ad6
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-25 00:18:22 +0200

    Update registerController.php

--- controllers/registerController.php
+++ controllers/registerController.php
@@ -13,13 +13,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Verifique se as senhas coincidem
     if ($password === $confirm_password) {
-   
-        header('Location: login.php');
+
+
 
         $conn = new mysqli('localhost', 'root', '', 'fatima_zologico');
+        $dateTime = new DateTime();
+        $criado_em = $dateTime->format('Y-m-d H:i:s');
 
-        $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+        $query = "INSERT INTO users (username,email,sexo,datanascimento,telefone,Cidade,password,criado_em)
+         VALUES ('$username', '$email','$sexo','$datanascimento','$telefone','$Cidade','$password','$criado_em')";
         if ($conn->query($query) === TRUE) {
+
+            setcookie('username', $username, time() + 3600, '/');
+            setcookie('email', $email, time() + 3600, '/');
+
+            header('Location: ../src/pages/dashboard/index.php');
             echo "Registro bem-sucedido!";
         } else {
             echo "Erro ao registrar: " . $conn->error;

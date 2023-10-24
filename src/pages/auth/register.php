945867e7f7c571307c9d4f98165b41e2ad93b7bf
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-25 01:14:40 +0200

    Update register.php

--- src/pages/auth/register.php
+++ src/pages/auth/register.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['error_message'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../assets/css/cadastro.css">
-    <title>Document</title>
+    <title>Registar</title>
 </head>
 
 <body>

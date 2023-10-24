173d2f631509f37c02875d0e5792b65c8c6ab90f
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-25 01:14:35 +0200

    Update login.php

--- src/pages/auth/login.php
+++ src/pages/auth/login.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['error_message'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="../../../assets/css/login.css">
 </head>
 

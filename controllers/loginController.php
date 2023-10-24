1f423d220f1fb52e37a1eadede75de7cab7dcfa7
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-25 01:13:49 +0200

    Update loginController.php

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,17 +1,15 @@
 <?php
 session_start();
-include '../configs/database.php'; // Inclua o arquivo de conexão
+include '../configs/database.php'; 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Use a conexão com o banco de dados para verificar o nome de usuário e a senha
     $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
     $result = $conn->query($query);
 
     if ($result->num_rows == 1) {
-        // Login bem-sucedido, agora defina os cookies com o email e o nome de usuário
         $row = $result->fetch_assoc();
         $email = $row['email'];
 
@@ -19,11 +17,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         setcookie('email', $email, time() + 3600, '/');
 
         $_SESSION['username'] = $username;
-        header('Location: painel.php'); // Redirecionar para a página do painel após o login
+
+        header('Location: ../src/pages/dashboard/index.php');
     } else {
-        // Senhas não coincidem, defina uma mensagem de erro na sessão
         $_SESSION['error_message'] = "Nome de usuário ou senha incorretos";
-        // Redirecione de volta à página anterior
         header('Location: ../src/pages/auth/login.php');
     }
 }

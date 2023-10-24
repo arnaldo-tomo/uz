6dc0ffe9bfcd034724fa6671a9120aea2b5aa36e
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-25 01:14:55 +0200

    Create repteis.php

---
+++ src/pages/grupo/repteis.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Repteis</title>
+    <style>
+        .animal {
+             text-align: center;
+             margin: 20px;
+         }
+ 
+         .animal img {
+             max-width: 300px;
+             max-height: 200px;
+         }
+ 
+         .animal-table {
+             border: 1px solid #ccc;
+             border-collapse: collapse;
+             width: 80%;
+             margin: 0 auto;
+         }
+ 
+         .animal-table th, .animal-table td {
+             border: 1px solid #ccc;
+             padding: 10px;
+         }
+     </style>
+</head>
+<body>
+    <P>Repteis</P>
+    <div class="animal">
+        <h2>Girafa</h2>
+        <img src="" alt="Curuja">
+        <table class="animal-table">
+            <tr>
+                <th>Especie</th>
+                <td>Tyto Alba</td>
+            </tr>
+            <tr>
+                <th>Habitat</th>
+                <td>Savanas e florestas da África</td>
+            </tr>
+            <tr>
+                <th>Alimentação</th>
+                <td>Herbívora</td>
+            </tr>
+            <tr>
+                <th>Gestacao</th>
+                <td>4 meses</td>
+            </tr>
+            <tr>
+                <th>Peso</th>
+                <td> 33 e 36 cm</td>
+            </tr>
+            <tr>
+                <th>Sexo</th>
+                <td>Fêmea</td>
+            </tr>
+            <tr>
+                <th>Data de Aquisição</th>
+                <td>5 de março de 2022</td>
+            </tr>
+            <tr>
+                <th>Expectativa de Vida</th>
+                <td>20-25 anos</td>
+            </tr>
+            <tr>
+                <th>Detalhes</th>
+                <td>A girafa é conhecida por seu pescoço longo e elegante, usado para alcançar folhas altas nas árvores.</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>

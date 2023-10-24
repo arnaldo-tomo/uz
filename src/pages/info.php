1eae782390d4113401f6e5164001ec562bf3cb90
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-25 01:15:01 +0200

    Update info.php

--- src/pages/info.php
+++ src/pages/info.php
@@ -1,28 +1,218 @@
+<?php
+session_start();
+
+// Verifique se os cookies estão definidos
+if (isset($_COOKIE['username']) && isset($_COOKIE['email'])) {
+    $username = $_COOKIE['username'];
+    $email = $_COOKIE['email'];
+} else {
+    $_SESSION['error_message'] = "Voce nao esta autenticado.";
+    // Os cookies não estão definidos, redirecione para a página de login
+    header('Location: ../../index.php');
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Informacao Geral</title>
-    <link rel="stylesheet" href="../../assets/css/MainStyle.css">
+    <title>ZOOLÓGICO - <?php echo $username; ?></title>
 
+    <link rel="stylesheet" type="text/css" href="../../assets/css/back.css">
+    <link rel="stylesheet" href="../../assets/css/geral.css">
+    <link rel="stylesheet" href="../../assets/css/MainStyle.css">
+    <link rel="stylesheet" href="../../assets/css/fatima.css">
+    <link rel="stylesheet" href="../../assets/css/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <style>
+        footer {
+            background-color: #e2c044 color: #fff;
+            text-align: center;
+            padding: 10px 0;
+            margin-top: 20px;
+        }
+
+
+
+        .cor {
+            text-decoration-color: white;
+        }
+    </style>
 </head>
 
 <body>
-    <section class="informacao-geral">
-        <div class="conteudo1">
-            <h2>Informação Geral do Zoológico</h2>
-            <p><strong>Horário de Funcionamento:</strong> Segunda a Domingo, das 9:00 às 18:00</p>
-            <p><strong>Preço de Ingresso:</strong> $20 (adultos), $10 (crianças de 3 a 12 anos)</p>
-            <p><strong>Serviços Oferecidos:</strong> Alimentação, Passeios Guiados, Lojas de Lembranças</p>
-            <p><strong>Endereço:</strong> 123 Rua do Zoológico, Cidade da Beira</p>
-            <p><strong>Telefone:</strong> (+258)844604568</p>
-            <p><strong>Diretor:</strong> Nome do Diretor</p>
-            <p><strong>Email para Contato:</strong> josesixpence30@gmail.com</p>
-            <p><strong>Número de Visitantes por Ano:</strong> Cerca de 500.000</p>
+    <div id="content">
+        <!--=============== HEADER ===============-->
+        <header class="header">
+            <nav class="nav container">
+                <div class="nav__data">
+                    <a href="./info.php" class="nav__logo">
+                        <i class="ri-planet-line"></i> ZOOlandia
+                    </a>
+
+                    <div class="nav__toggle" id="nav-toggle">
+                        <i class="ri-menu-line nav__burger"></i>
+                        <i class="ri-close-line nav__close"></i>
+                    </div>
+                </div>
+
+                <!--=============== NAV MENU ===============-->
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li><a href="./info.php" class="nav__link">Home</a></li>
+
+                        <li class="ative"><a href="info.php" class="nav__link ">Sobre</a></li>
+
+                        <!--=============== DROPDOWN 1 ===============-->
+                        <li class="dropdown__item">
+                            <div class="nav__link">
+                                Grupos <i class="ri-arrow-down-s-line dropdown__arrow"> </i>
+                            </div>
+
+                            <ul class="dropdown__menu">
+                                <li>
+                                    <a href="grupos.php" class="dropdown__link">
+                                        <i class="ri-pie-chart-line"></i> Todos os Grupos
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="grupo/Mamiferos.php" class="dropdown__link">
+                                        <i class="ri-pie-chart-line"></i> Mamíferos
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="grupo/repteis.php" class="dropdown__link">
+                                        <i class="ri-arrow-up-down-line"></i> Repteis
+                                    </a>
+                                </li>
+
+                                <!--=============== DROPDOWN SUBMENU ===============-->
+                                <li class="dropdown__subitem">
+                                    <div class="dropdown__link">
+                                        <i class="ri-bar-chart-line"></i> Aves -> <i class="ri-add-line dropdown__add"></i>
+                                    </div>
+
+                                    <ul class="dropdown__submenu">
+                                        <li>
+                                            <a href="grupo/aves.php" class="dropdown__sublink">
+                                                <i class="ri-file-list-line"></i> Aves colocar nome
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <a href="#" class="dropdown__sublink">
+                                                <i class="ri-cash-line"></i> Outro nome
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <a href="#" class="dropdown__sublink">
+                                                <i class="ri-refund-2-line"></i> aqui tamben
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+
+
+                        <!--=============== DROPDOWN 2 ===============-->
+                        <li class="dropdown__item">
+                            <div class="nav__link">
+                                Ola, <?php echo $username; ?> <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                            </div>
+
+                            <ul class="dropdown__menu">
+                                <li>
+                                    <a href="#" class="dropdown__link">
+                                        <i class="ri-user-line"></i> <?php echo $email; ?>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="dropdown__link">
+                                        <i class="ri-lock-line"></i> Configuração
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="../../configs/logout.php" class="dropdown__link">
+                                        <i class="ri-message-3-line"></i> Log Out
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+
+        <form action="pagina-de-resultado.html" method="get">
+            <!-- <input type="text" id="pesquisa" name="q" placeholder="Pesquisar..." autofocus> -->
+            <!-- <input type="submit" value="Pesquisar"> -->
+            <section class="fatima22">
+                <h1>Todos os Animas</h1>
+            </section>
+
+            <section>
+                <div class="conteudo1">
+                    <h1 class="cor">Escrever aqui a vossa descricao</h1>
+                    <p class="cor">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi esse doloribus
+                        necessitatibus
+                        dolorum! Nisi doloremque quidem error voluptatem omnis consectetur sapiente iusto quas mollitia
+                        repellat. Harum ab laborum illum perferendis.
+                        locale_accept_from_httpl Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+                        quaerat dolore dolorem quia numquam temporibus consequatur beatae sapiente quis praesentium
+                        recusandae ea doloribus saepe, deleniti aliquid nobis aliquam ad veritatis?
+                        Sunt quas dolorum explicabo libero vero veniam eum nemo, ea enim amet doloribus aliquid illo
+                        necessitatibus? Corporis quidem sit cum, dolores repellendus repudiandae sapiente, quae
+                        blanditiis non maxime architecto expedita.
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt quis reprehenderit sapiente
+                        distinctio unde vel veniam ducimus voluptatibus dolore aspernatur, quia nisi tempore, nam
+                        repudiandae error expedita et ea accusamus?
+
+                    </p>
+
+                </div>
+            </section>
+
+        </form>
+    </div>
+    <footer>
+        <div class="rodape-container">
+            <div class="informacoes-contato">
+                <h3>Entre em Contato</h3>
+                <p><strong>Endereço:</strong> Rua do Zoológico, 1234</p>
+                <p><strong>Telefone:</strong> (123) 456-7890</p>
+                <p><strong>E-mail:</strong> contato@seuzoo.com</p>
+            </div>
+            <div class="redes-sociais">
+                <h3>Redes Sociais</h3>
+                <a href="https://twitter.com/seu-usuario-do-twitter" class="social-icon">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="https://www.instagram.com/seu-usuario-do-instagram/" class="social-iconi">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.facebook.com/sua-pagina-do-facebook" class="social-iconf">
+                    <i class="fab fa-facebook"></i>
+                </a>
+            </div>
         </div>
-    </section>
+        <div class="direitos-autorais">
+            &copy; 2023 Seu Zoológico. Todos os direitos reservados.
+        </div>
+    </footer>
+    <script src="../../assets/js/fatima.js"></script>
+
+    </div>
+    <script src="../../assets/js/script.js"></script>
 </body>
 
 </html>

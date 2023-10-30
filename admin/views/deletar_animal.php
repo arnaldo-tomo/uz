cf43432b913a86b3647e559d884368fd09e56989
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-31 01:39:04 +0200

    Create deletar_animal.php

---
+++ admin/views/deletar_animal.php
@@ -0,0 +1,23 @@
+<?php
+include '../../configs/database.php';
+// Verifique se o ID do animal a ser excluído foi passado na URL
+if (isset($_GET['id'])) {
+    $animal_id = $_GET['id'];
+
+
+
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
+    // Exclua o registro do animal
+    $sql = "DELETE FROM animal WHERE id = $animal_id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Animal excluído com sucesso.";
+    } else {
+        echo "Erro ao excluir o animal: " . $conn->error;
+    }
+} else {
+    echo "ID do animal não especificado.";
+}

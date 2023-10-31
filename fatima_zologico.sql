cd761a4708ba11f94b71c338eeb405e8650c1b81
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-01 00:01:08 +0200

    Update fatima_zologico.sql

--- fatima_zologico.sql
+++ fatima_zologico.sql
@@ -27,7 +27,7 @@ CREATE TABLE `admin` (
   `root` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +38,82 @@ LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 INSERT INTO `admin` VALUES (1,'admin','admin');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `animal`
+--
+
+DROP TABLE IF EXISTS `animal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `descricao` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
+  `sexo` varchar(255) DEFAULT NULL,
+  `foto` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
+  `especie` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 ;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animal`
+--
+
+LOCK TABLES `animal` WRITE;
+/*!40000 ALTER TABLE `animal` DISABLE KEYS */;
+INSERT INTO `animal` VALUES (10,'Zembra','Felex Desenvolvi  em moçamboque','M','zebras_234779578.jpg','rato'),(11,'Macaco','macamoc descricao','F','01-animals-social-distancing-nationalgeographic_110344.jpg','rato');
+/*!40000 ALTER TABLE `animal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `especie`
+--
+
+DROP TABLE IF EXISTS `especie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `especie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `descricao` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 ;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especie`
+--
+
+LOCK TABLES `especie` WRITE;
+/*!40000 ALTER TABLE `especie` DISABLE KEYS */;
+INSERT INTO `especie` VALUES (4,'rato','bbb'),(5,'Arnaldo Tomo','descric'),(6,'Leão (Panthera leo)','Um grande felino da família Felidae.'),(7,'Orca (Orcinus orca)','Uma espécie de golfinho que habita oceanos em todo o mundo.'),(8,'Tigre (Panthera tigris)','Outro grande felino, conhecido por sua pelagem listrada.'),(9,'Baleia-azul (Balaenoptera musculus) ',' O maior animal do planeta, uma espécie de baleia.');
+/*!40000 ALTER TABLE `especie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sexo`
+--
+
+DROP TABLE IF EXISTS `sexo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sexo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sexo`
+--
+
+LOCK TABLES `sexo` WRITE;
+/*!40000 ALTER TABLE `sexo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sexo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -58,7 +134,7 @@ CREATE TABLE `users` (
   `datanascimento` varchar(45) DEFAULT NULL,
   `criado_em` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 ;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +146,33 @@ LOCK TABLES `users` WRITE;
 INSERT INTO `users` VALUES (1,'hh','nado','arnaldotomo@gamil.com','masculino','+258846474687','Beira','2023-10-25','2023-10-24 20:04:29'),(2,'arnaldotomo','12','arnaldotomo@gmail.com','masculino','+258846474687','Beira','2023-10-28','2023-10-24 20:13:05'),(3,'celsojuliojose@gmail.com','n','arnaldotomo@gamil.com','feminino','+258846474687','Beira','2023-10-13','2023-10-24 21:20:43'),(4,'arnaldotomo','1','admin@gmail.com','masculino','555','Beira','2023-10-27','2023-10-24 22:06:02');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `visitas`
+--
+
+DROP TABLE IF EXISTS `visitas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `visitas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(225) CHARACTER SET utf8mb4  DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
+  `datahora` varchar(255) DEFAULT NULL,
+  `pessoas` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 ;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visitas`
+--
+
+LOCK TABLES `visitas` WRITE;
+/*!40000 ALTER TABLE `visitas` DISABLE KEYS */;
+INSERT INTO `visitas` VALUES (3,' hh','arnaldotomo@gamil.com','2023-10-26T12:27',2),(4,' hh','arnaldotomo@gamil.com','2023-10-31T12:33',12),(5,' hh','arnaldotomo@gmil.com','2023-10-31T12:32',2);
+/*!40000 ALTER TABLE `visitas` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -80,4 +183,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-25 16:51:18
+-- Dump completed on 2023-10-31 13:44:14

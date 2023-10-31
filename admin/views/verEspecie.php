9d0ac0f9b55a2f9c49d7660dff553863536b736b
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-31 12:44:13 +0200

    Update verEspecie.php

--- admin/views/verEspecie.php
+++ admin/views/verEspecie.php
@@ -121,29 +121,14 @@ $result = $conn->query($sql);
                 <!-- End -->
 
                 <li class="item">
-                    <a href="#" class="nav_link">
+                    <a href="./bilhetes.php" class="nav_link">
                         <span class="navlink_icon">
                             <i class='bx bxs-door-open'></i>
                         </span>
-                        <span class="navlink">Bilhentes</span>
+                        <span class="navlink">Reservas</span>
                     </a>
                 </li>
-                <li class="item">
-                    <a href="#" class="nav_link">
-                        <span class="navlink_icon">
-                            <i class="bx bx-loader-circle"></i>
-                        </span>
-                        <span class="navlink">Outros</span>
-                    </a>
-                </li>
-                <li class="item">
-                    <a href="#" class="nav_link">
-                        <span class="navlink_icon">
-                            <i class="bx bx-loader-circle"></i>
-                        </span>
-                        <span class="navlink">Outros</span>
-                    </a>
-                </li>
+
             </ul>
 
             <!-- Sidebar Open / Close -->
